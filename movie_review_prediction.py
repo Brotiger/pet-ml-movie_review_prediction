dc5da52a9318a980ac4d869f6c0a61a7c3319321
Дмитрий Берестнев <dberestnev@rteam.tech>
2023-07-10 20:13:43 +0300

    Добавлен поиск 3 отзывов наиболее схожих с заданным отзывом

--- movie_review_prediction.py
+++ movie_review_prediction.py
@@ -139,5 +139,41 @@ def analogy(positive=['terrible', 'good'], negative=['bad']):
     
     return scores.most_common(10)[1:]
 
+print()
 print(analogy(['elizabeth', 'he'], ['she']))
 print(analogy(['terrible', 'good'], ['bad']))
+
+# Поиск 3 отзывов наиболее схожих с заданным отзывом
+norms = np.sum(weights_0_1 * weights_0_1, axis=1)
+norms.resize(norms.shape[0], 1)
+normed_weights = weights_0_1 * norms
+
+# Проверяем наличие слов в индексе
+# Если есть извлекаем их индекс
+# Берем средне арифметическое по указанной оси
+def make_sent_vect(words):
+    indices = list(map(lambda x:word2index[x], filter(lambda x:x in word2index, words)))
+    return np.mean(normed_weights[indices], axis=0)
+
+# Вычисляем средний вектор для каждого обзора
+reviews2vectors = list()
+for review in tokens:
+    reviews2vectors.append(make_sent_vect(review))
+reviews2vectors = np.array(reviews2vectors)
+
+def most_similar_reviews(review):
+    v = make_sent_vect(review)
+    scores = Counter()
+    
+    # Те вектора которые оказались наиболее большими при перемножении те и являются наиболее подходящими по смыслу
+    for i, val in enumerate(reviews2vectors.dot(v)):
+        scores[i] = val
+        
+    most_similar = list()
+    
+    for idx, score in scores.most_common(3):
+        most_similar.append(raw_reviews[idx][0:40])
+    return most_similar
+
+print()
+print(most_similar_reviews(['boring', 'awful']))
